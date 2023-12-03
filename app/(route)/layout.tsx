b10e718737905c332afe33b12f248dfbcfb5ccf6
jybaek96 <qorwjddus96@naver.com>
2023-12-03 14:19:40 +0900

    chore: tailwind 클래스명 정렬 플러그인 적용 (#1)
    tailwind css 클래스명 정렬 플러그인 (prettier-plugin-tailwindcss) 적용
    플러그인 적용 버그 해결을 위한 prettier 버전 downgrade

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-white dark:bg-black">
+      <body className="bg-white p-2 pt-2 dark:bg-black">
         <ThemeSwitcher />
         <Providers>{children}</Providers>
       </body>
